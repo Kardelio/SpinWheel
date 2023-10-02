de8d652af39c11fbc0229fed52301dae155bf209
kardelio <kardeliogithub@outlook.com>
2023-10-02 17:03:56 +0200

    Initial commit

---
+++ SpinWheel/src/main/java/com/kardelio/spinwheel/WheelSegment.kt
@@ -0,0 +1,10 @@
+package com.kardelio.spinwheel
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+class WheelSegment(
+    val text: String,
+    val backgroundBrush: Brush,
+    val textColor: Color
+)
